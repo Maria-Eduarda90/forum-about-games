27685a86394da8d763de3409ffa4acf689960cce
Mary Skywalker <snowdennaruto90@gmail.com>
2022-08-22 18:17:24 -0400

    try/catch in AuthController

--- backend/app/Controllers/Http/AuthController.ts
+++ backend/app/Controllers/Http/AuthController.ts
@@ -3,30 +3,43 @@ import User from 'App/Models/User'
 
 export default class AuthController {
   public async login({ request, auth }: HttpContextContract){
-    const email = request.input('email');
-    const password = request.input('password');
+    try {
+      const email = request.input('email');
+      const password = request.input('password');
 
-    const token = await auth.use('api').attempt(email, password, {
-      expiresIn: '1day'
-    });
-    return token.toJSON()
+      const token = await auth.use('api').attempt(email, password, {
+        expiresIn: '1day'
+      });
+
+      return token.toJSON()
+    } catch (err){
+      console.log(err);
+    }
   }
 
   public async register({ request }: HttpContextContract){
-    const { name, email, password } = request.only(['name', 'email', 'password']);
+    try {
+      const { name, email, password } = request.only(['name', 'email', 'password']);
 
-    const createUser = await User.create({
-      name,
-      email,
-      password,
-    })
+      const createUser = await User.create({
+        name,
+        email,
+        password,
+      })
 
-    return createUser;
+      return createUser
+    } catch(err){
+      console.log(err);
+    }
   }
 
   public async index(){
-    const user = await User.all();
+    try {
+      const user = await User.all();
 
-    return user;
+      return user;
+    } catch(err){
+      console.log(err);
+    }
   }
 }
