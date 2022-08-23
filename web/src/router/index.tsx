08730609b170ab76da66fd22bc83b28933439d83
Mary Skywalker <snowdennaruto90@gmail.com>
2022-08-22 23:17:55 -0400

    adding rota signup

--- web/src/router/index.tsx
+++ web/src/router/index.tsx
@@ -1,11 +1,13 @@
-import { BrowserRouter as Router, Routes, Route, Outlet, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Login } from '../pages/Login';
+import { Signup } from '../pages/Signup';
 
 export function AppRouter(){
     return(
         <Router>
             <Routes>
-                <Route path="/" element={<Login/>}/>
+                <Route path="/" element={<Login />} />
+                <Route path="/cadastrar" element={<Signup />} />
             </Routes>
         </Router>
     );
