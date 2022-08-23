311ddb06b2fa8873e015c9616e51460fa143a6a0
Mary Skywalker <snowdennaruto90@gmail.com>
2022-08-22 23:19:14 -0400

    signup screeen completed

---
+++ web/src/pages/Signup/index.tsx
@@ -0,0 +1,29 @@
+import { Button } from '../../components/Button';
+import { Input } from '../../components/Input';
+import styles from './styles.module.scss';
+
+import iconImg from '../../img/680ed9ad3e25665eb9f5d9f598a119b7.jpg';
+import { Link } from 'react-router-dom';
+
+export function Signup() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.wrapper}>
+                <form>
+                    <img src={iconImg} alt="controle ps" />
+                    <h1>WELCOME!</h1>
+                    <p>Bem vindo ao fórum sobre Games 🎮</p>
+                    <Input type={'text'} placeholder={'Username'} />
+                    <Input type={'email'} placeholder={'E-mail'} />
+                    <Input type={'password'} placeholder={'Password'} />
+                    <Input type={'url'} placeholder={'url da sua foto (opcional)'} />
+                    <Button>
+                        Signup
+                    </Button>
+
+                    <span>já possui uma conta?<Link to="/">Signup</Link></span>
+                </form>
+            </div>
+        </div>
+    );
+}
