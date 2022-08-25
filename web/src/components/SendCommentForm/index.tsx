8e27eda7fd10669d00a1adaa3f40568a5e5ee73f
Mary Skywalker <snowdennaruto90@gmail.com>
2022-08-24 21:50:59 -0400

    updated

--- web/src/components/SendCommentForm/index.tsx
+++ web/src/components/SendCommentForm/index.tsx
@@ -1,16 +1,26 @@
+import { useState } from 'react';
 import styles from './styles.module.scss';
 
+const MAX_COMMENT_CHAR = 200;
+
 export function SendCommentForm(){
+    const [text, setText] = useState('');
+
     return(
         <div className={styles.container}>
             <form action="">
-                <textarea name="text" placeholder="Comente algo sobre o mundo dos jogos  ¯\_(ツ)_/¯"/>
+                <textarea
+                 name="text" 
+                 placeholder="Comente algo sobre o mundo dos jogos  ¯\_(ツ)_/¯"
+                 value={text}
+                 onChange={e => setText(e.target.value)}
+                />
 
                 <div>
                     <span>
-                        <span>0</span> / <span className={styles.count}>140</span>
+                        <span>{text.length}</span> / <span className={styles.count}>{MAX_COMMENT_CHAR}</span>
                     </span>
-                    <button>Comentar</button>
+                    <button disabled={text.length > MAX_COMMENT_CHAR}>Comentar</button>
                 </div>
             </form>
         </div>
