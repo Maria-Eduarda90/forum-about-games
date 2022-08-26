ba87c48c977c47e7bedca3109121167028825349
Mary Skywalker <snowdennaruto90@gmail.com>
2022-08-25 22:32:06 -0400

    updated page games

---
+++ web/src/components/CarouselGames/index.tsx
@@ -0,0 +1,7 @@
+import styles from './styles.module.scss';
+
+export function CarouselGames(){
+    return(
+        <h1></h1>
+    );
+}
--- web/src/pages/Games/index.tsx
+++ web/src/pages/Games/index.tsx
@@ -1,9 +1,15 @@
 import { NavBar } from "../../components/NavBar";
+import styles from './styles.module.scss';
 
 export function Games(){
     return(
         <>
             <NavBar/>
+
+            <div className={styles.title}>
+                <h1>Melhores Jogos!</h1>
+                <p>Na opinião dessa mera desenvolvedora ;-)</p>
+            </div>
         </>
     );
 }
