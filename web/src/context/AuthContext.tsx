544b08781c964dc19ce4e2ca138375850d3177a1
Mary Skywalker <snowdennaruto90@gmail.com>
2022-08-28 19:03:53 -0400

    added context

---
+++ web/src/context/AuthContext.tsx
@@ -0,0 +1,59 @@
+import { createContext, ReactNode, useState } from 'react';
+import api from '../api/api';
+import { SignInProps } from '../data/@types/SignIn';
+import { SignUpProps } from '../data/@types/SignUp';
+import { UserProps } from '../data/@types/User';
+
+type ContextData = {
+    user: UserProps | null;
+    userSignIn: (userData: SignInProps) => Promise<UserProps>;
+    userSignUp: (userData: SignUpProps) => Promise<UserProps>;
+    getCurrentUser: () => Promise<UserProps>;
+}
+
+type ElementChildren = {
+    children: ReactNode;
+}
+
+export const AuthContext = createContext<ContextData>({} as ContextData)
+
+export const AuthProvider = ({ children }: ElementChildren) => {
+    const [user, setUser] = useState<UserProps | null>(null);
+
+    const userSignIn = async (userData: SignInProps) => {
+        localStorage.clear();
+        const { data } = await api.post("api/login", userData);
+
+        if (data.token) {
+            localStorage.setItem('@Forum:Token', data.token);
+        }
+
+        return getCurrentUser();
+    }
+
+    const getCurrentUser = async () => {
+        const { data } = await api.get("api/user")
+        setUser(data)
+        localStorage.setItem('@Forum:User', JSON.stringify(user));
+        return data;
+    }
+
+    const userSignUp = async (userData: SignUpProps) => {
+        const { data } = await api.post("api/user", userData);
+
+        return data;
+    }
+
+    return (
+        <AuthContext.Provider value={
+            {
+                user,
+                userSignIn,
+                getCurrentUser,
+                userSignUp,
+            }
+        }>
+            {children}
+        </AuthContext.Provider>
+    );
+}
