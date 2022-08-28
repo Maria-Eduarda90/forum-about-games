928d6acd28231518903357d76b41bc8cd344d6b9
Mary Skywalker <snowdennaruto90@gmail.com>
2022-08-28 18:58:28 -0400

    api call

--- web/src/api/api.ts
+++ web/src/api/api.ts
@@ -1,5 +1,17 @@
 import axios from 'axios';
 
-export const api = axios.create({
-    baseURL: '',
-})
+const api = axios.create({
+  baseURL: "http://127.0.0.1:3333",
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("@Forum:Token") || "";
+  config.headers = {
+    Authorization: `Bearer ${token}`,
+    "Content-Type": "application/json",
+  };
+
+  return config;
+});
+
+export default api;
