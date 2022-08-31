2fba496f72f19eb9eb9a7902229407447e2f4f05
Mary Skywalker <snowdennaruto90@gmail.com>
2022-08-30 21:35:01 -0400

    if ternario in component [comment]

--- web/src/components/Comments/index.tsx
+++ web/src/components/Comments/index.tsx
@@ -4,14 +4,14 @@ import styles from './styles.module.scss';
 
 export function Comments(){
     const { comment } = useComment();
-    console.log(comment);
+
     return(
         <>
             {comment.map(index => {
                 return(
                     <div className={styles.container} key={index.id}>
                         <div className={styles.image}>
-                            <img src={index.user.avatar ?? imgAvatar} alt="foto do usuario" />
+                            <img src={index.user.avatar ? index.user.avatar : imgAvatar} alt="foto do usuario" />
                         </div>
                         <div className={styles.content}>
                             <span className={styles.name}>{index.user.name}</span>
